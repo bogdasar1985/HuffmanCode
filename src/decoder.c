90340f17bd98877b8e35257fb71dc9af5e5622b8
bogdasar1985 <bogdasar1985@gmail.com>
2020-10-01 20:25:50 +0500

    Fix a bug with less bits!

--- src/decoder.c
+++ src/decoder.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/stat.h>
 #include "utilities.h"
 #include "huffman.h"
 int main(int argc, char *argv[])
@@ -12,6 +13,7 @@ int main(int argc, char *argv[])
     char ch = 0x0;
     char code[CHAR_BIT] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
     size_t pos = 0;
+    struct stat stats;
 
     if((fl = fopen(argv[1], "r")) == NULL)
     {
@@ -38,25 +40,39 @@ int main(int argc, char *argv[])
         insert_ready_node(&tree, tmp);
     }
     
-    while(fread(&ch, 1, 1, fl) != 0)
+    fstat(fileno(fl), &stats);
+    for(long j = ftell(fl); j < stats.st_size; ++j)
     {
         struct node *tmp = NULL;
-        for(int i = CHAR_BIT-1; i >= 0; --i)
+        fread(&ch, 1, 1, fl);
+        if(j ==  stats.st_size - 1)
         {
-            // Считываем биты в массив
-            code[pos] = ((ch & (1 << i)) >> i) + '0';
-            ++pos;
-            #ifdef DEBUG
-            fprintf(stdout, "Get bit %d to code. Code is %s\n", code[pos-1], code);
-            #endif
-            if((tmp = get_symbol(tree, code)) != NULL)
+            for(int i = CHAR_BIT-1-less_bits; i >= 0; --i)
             {
-                #ifdef DEBUG
-                fprintf(stdout, "   Code %s is a symbol -->%c<--\n", code, tmp->symbol);
-                #endif
-                fwrite(&(tmp->symbol), 1, 1, fl_write);
-                memset(code, '\0', CHAR_BIT);
-                pos = 0;
+                // Считываем биты в массив
+                code[pos] = ((ch & (1 << i)) >> i) + '0';
+                ++pos;
+                if((tmp = get_symbol(tree, code)) != NULL)
+                {
+                    fwrite(&(tmp->symbol), 1, 1, fl_write);
+                    memset(code, '\0', CHAR_BIT);
+                    pos = 0;
+                }
+            }
+        }
+        else
+        {
+            for(int i = CHAR_BIT-1; i >= 0; --i)
+            {
+                // Считываем биты в массив
+                code[pos] = ((ch & (1 << i)) >> i) + '0';
+                ++pos;
+                if((tmp = get_symbol(tree, code)) != NULL)
+                {
+                    fwrite(&(tmp->symbol), 1, 1, fl_write);
+                    memset(code, '\0', CHAR_BIT);
+                    pos = 0;
+                }
             }
         }
     }
